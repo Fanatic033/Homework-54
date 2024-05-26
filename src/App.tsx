8eacb0be46cfa717241c4a2acef4c4a8d5172727
ILzat <ilzat.sadykov.2006@bk.ru>
2024-05-27 00:00:29 +0600

    " Добавил кнопку reset где мы сбрасываем стейт до нуля "

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import Board from './components/Board/Board.tsx';
 import './App.css'
 import './main.css'
 import AttemptsCounter from './components/Attempts/Attempts.tsx';
+import ResetButton from './components/ResetButton/ResetButton.tsx';
 
 const App: React.FC = () => {
   const [items, setItems] = useState(createItems());
   const [attempts, setAttempts] = useState(0);
-    const UserClick = (index: number) => {
+  const Reset = (): void => {
+    setItems(createItems());
+    setAttempts(0);
+  };
+
+  const UserClick = (index: number) => {
       if (!items[index].clicked) {
         const newItems = items.map((item, i) =>
           i === index ? { ...item, clicked: true } : item
@@ -22,6 +28,7 @@ const App: React.FC = () => {
       <h1>Найди собачку</h1>
       <Board items={items} onCellClick={UserClick}/>
       <AttemptsCounter attempts={attempts} />
+      <ResetButton onReset={Reset} />
     </>
   );
 };
